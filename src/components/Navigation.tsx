8238fac2ef3304bf259493676a8c1eac5997242f
Meet Bhatt <31917173+bhattmeet@users.noreply.github.com>
2025-11-29 23:27:31 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import logo from "/public/codenest-logo.jpeg";
+import logo from "@/assets/codenest-logo.jpeg";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
